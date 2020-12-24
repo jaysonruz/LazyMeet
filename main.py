5513ad9260461c60af0b0ee44b2dc4659d047244
jayson <jaysonruzario@gmail.com>
2020-12-25 02:38:37 +0530

    all functionality is completed for settings tab

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-# from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QTextEdit ,QMessageBox ,QLineEdit ,QFileDialog , QDateEdit 
-from PyQt5 import uic
-# import sys
-# from PyQt5.QtCore import QSettings 
-
-from backports import configparser
-
-from configManager import ConfigManager
-
+##imports
 from PyQt5.QtWidgets import * 
 from PyQt5 import QtCore, QtGui 
+from PyQt5 import uic
 from PyQt5.QtGui import * 
 from PyQt5.QtCore import * 
 import sys 
 
+from backports import configparser
 
+from configManager import ConfigManager
+#----------------------------------------------------------
 class MyApp(QMainWindow):
     def __init__(self):
         super(MyApp, self).__init__()
@@ -22,7 +18,7 @@ class MyApp(QMainWindow):
         self.settings = QSettings('LazyMeet','App state')
         self.configManager = ConfigManager()
         self.config = configparser.ConfigParser()
-
+        
         try:
             ####
             #loads application previous state like size n position 
@@ -41,8 +37,7 @@ class MyApp(QMainWindow):
         self.button_reset.clicked.connect(self.onReset)
         self.button_Browse = self.findChild(QPushButton,"pushButton_Browse")
         self.button_Browse.clicked.connect(self.change_cookie_directory)
-        
-        self.DateEdit = self.findChild(QDateEdit,"dateEdit")
+
 
     def dateToQdate(self,_date):
         '''
@@ -50,56 +45,68 @@ class MyApp(QMainWindow):
         ''' 
         d=_date.split("-")
         return QDate(int(d[0]),int(d[1]),int(d[2]))
-
-      
+    def timeTOQtime(self,_time):
+        '''
+        takes time of type str eg.04:03:00 and return Qtime(04,03,00)
+        '''
+        d=_time.split(":")
+        return QTime(int(d[0]),int(d[1]),int(d[2]))
 
     def updateconfig(self):
         #####
         #Loads userconfig from file if it is available 
         #####
         self.config.read('config.ini')
+        #----defining interface
         self.lineEdit_cookieDirectory = self.findChild(QLineEdit,"lineEdit_cookieDirectory")
-        print(self.lineEdit_cookieDirectory.text())
-        self.lineEdit_cookieDirectory.setText(self.config["UserConfig"]["cookiedirectory"])
-
         self.lineEdit_meetingLink = self.findChild(QLineEdit,"lineEdit_meetingLink")
-        print(self.lineEdit_meetingLink.text())
-        self.lineEdit_meetingLink.setText(self.config["UserConfig"]["googlemeetlink"])
-
         self.DateEdit = self.findChild(QDateEdit,"dateEdit")
+        self.timeEdit_Start = self.findChild(QTimeEdit,"timeEdit_Start")
+        self.timeEdit_End = self.findChild(QTimeEdit,"timeEdit_End")
+        #----update interface
+        self.lineEdit_cookieDirectory.setText(self.config["UserConfig"]["cookiedirectory"])
+        self.lineEdit_meetingLink.setText(self.config["UserConfig"]["googlemeetlink"])
         self.DateEdit.setDate(self.dateToQdate(self.config["UserConfig"]['date']))
+        self.timeEdit_Start.setTime(self.timeTOQtime(self.config["UserConfig"]['starttime']))
+        self.timeEdit_End.setTime(self.timeTOQtime(self.config["UserConfig"]['endtime']))
 
     def onApply(self):
         '''
         creates dictionary of values that user has provided and writes to config file using configmanager
         '''
-        #TODO implementing time and date to be stored in configfile
-        print(self.settings.fileName())
-        d = {'CookieDirectory': self.lineEdit_cookieDirectory.text(),'GoogleMeetLink': self.lineEdit_meetingLink.text(),'Date':self.DateEdit.date().toPyDate()}
+        d = {'CookieDirectory': self.lineEdit_cookieDirectory.text(),'GoogleMeetLink': self.lineEdit_meetingLink.text(),'Date':self.DateEdit.date().toPyDate(),
+            'startTime':self.timeEdit_Start.time().toString(),'endTime':self.timeEdit_End.time().toString()}
         self.configManager.setUserconfig(d)
         self.updateconfig()
-
+        self.pushButton_Apply.setText('Applied')
 
     def onReset(self):
         '''
-        deletes user config and resets it to default
+        overwrites user config reseting it to default
         '''
         self.configManager.resetConfig()
-        self.updateconfig()
+        try:
+            self.updateconfig()
+        except:
+            pass
 
     def change_cookie_directory(self):
-        self.datemanager()
+
         try:
             self.folder = str(QFileDialog.getExistingDirectory(None, "Select Directory",self.lineEdit_cookieDirectory.text()))
         except:
             self.folder = str(QFileDialog.getExistingDirectory(None, "Select Directory","c:/Users/"))
-        print(self.folder)
+
         self.lineEdit_cookieDirectory = self.findChild(QLineEdit,"lineEdit_cookieDirectory")
         self.lineEdit_cookieDirectory.setText(self.folder)
         self.cookie_directory = self.folder
 
 
     def closeEvent(self,event):
+        '''
+        this method is executed when exiting app
+        '''
+
         close = QMessageBox.question(self,
                                     "QUIT",
                                     "Are you sure want to stop process?",
